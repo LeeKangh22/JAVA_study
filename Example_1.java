0d2b7afdc405ac2b6beb0b96f03602353247313b
KANG HEE LEE <67950809+LeeKangh22@users.noreply.github.com>
2020-11-11 13:55:16 +0900

    Test 2

---
+++ Example_1.java
@@ -0,0 +1,14 @@
+
+public class Example_1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int num=0;
+		for(int i=0;i<10;i++) {
+			num+=i+1;
+			
+		}
+
+	}
+
+}
